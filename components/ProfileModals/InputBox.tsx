3e3e22345c21d3a07687eed2dd2767f91f0b41ea
Adejoke Ogunyinka <ogunyinkajoke@gmail.com>
2023-05-17 10:18:58 +0100

    view single post from user profile

--- components/ProfileModals/InputBox.tsx
+++ components/ProfileModals/InputBox.tsx
@@ -49,25 +49,29 @@ export const TextBox = ({
   id,
   placeholder,
   withoutBorder,
+  className,
 }: {
-  onChange: any;
-  onBlur: any;
+  onChange?: any;
+  onBlur?: any;
   value?: string;
-  title: string;
+  title?: string;
   id: string;
   placeholder: string;
   withoutBorder?: boolean;
+  className?: string;
 }) => {
   return (
     <div className="w-[100%]">
-      <label className="text-brand-200 font-medium text-[18px] leading-[162%]">
-        {title}
-      </label>
+      {title && (
+        <label className="text-brand-200 font-medium text-[18px] leading-[162%]">
+          {title}
+        </label>
+      )}
       <textarea
         placeholder={placeholder}
         className={`w-[100%] mt-[11px] h-[121px] rounded-[4px] ${
           withoutBorder ? "border-none" : "border-2 border-brand-2850"
-        } p-[25px] placeholder:text-brand-200 placeholder:text-[16px] focus:outline-0 focus:ring-offset-0 focus:ring-shadow-0 focus:outline-offset-0`}
+        } p-[25px] placeholder:text-brand-200 placeholder:text-[16px] focus:outline-0 focus:ring-offset-0 focus:ring-shadow-0 focus:outline-offset-0 ${className}`}
         id={id}
         onChange={onChange}
         onBlur={onBlur}
