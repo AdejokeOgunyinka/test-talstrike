a1326a6cc8df819053fa09cae7c0390a93bba69a
Adejoke Ogunyinka <ogunyinkajoke@gmail.com>
2023-05-28 15:03:55 +0100

    poll implementation

--- features/Auth/Signup/StarterVideo.tsx
+++ features/Auth/Signup/StarterVideo.tsx
@@ -1,17 +1,35 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 /* eslint-disable @next/next/no-html-link-for-pages */
 const StarterVideo = () => {
   const router = useRouter();
+
+  useEffect(() => {
+    const video = document.querySelector("video");
+
+    video?.addEventListener("ended", (event) => {
+      router.push("/auth/login");
+    });
+  });
+
   return (
     <div className="relative w-full h-screen flex justify-center">
-      <video
-        autoPlay
-        playsInline
+      <div
         className="w-full h-full object-cover"
-        src={"/talstrikeSportVideo.mp4"}
-        onEnded={() => router.push("/auth/login")}
-      />
+        dangerouslySetInnerHTML={{
+          __html: `
+        <video
+          id="talstrikeVideo"
+          muted
+          autoplay
+          playsinline
+          src="/talstrikeSportVideo.mp4"
+          class="w-full h-full object-cover"
+        />,
+      `,
+        }}
+      ></div>
 
       <div className="absolute right-[20px] top-[20px] text-[#fff] font-semibold">
         <a href="/auth/login">{"Skip >>"}</a>
