1a4006a417e6bb0c529c5704447224ab6fe35f89
Adejoke Ogunyinka <ogunyinkajoke@gmail.com>
2023-05-28 16:52:39 +0100

    poll implementation

--- features/Explore.tsx
+++ features/Explore.tsx
@@ -20,10 +20,43 @@ const Index = () => {
     vid?.pause();
   }
 
+  const exploreSections = [
+    { title: "For you" },
+    { title: "Trends" },
+    { title: "Top" },
+    { title: "Latest" },
+    { title: "Live" },
+  ];
+
+  const [currentSection, setCurrentSection] = useState(1);
+
   return (
     <DashboardLayout>
-      <div className="w-full flex justify-center md:rounded-tl-[15px] md:rounded-tr-[15px] min-h-[100vh] bg-brand-1000 py-[28px] px-[15px] lg:px-[31px]">
-        <div className="w-[90%] mt-[23px] flex flex-wrap gap-[12px] pb-[100px] lg:pb-0">
+      <div className="w-full flex flex-col justify-center md:rounded-tl-[15px] md:rounded-tr-[15px] min-h-[100vh] bg-brand-1000 py-[28px]">
+        <div className="flex z-[99] pl-[30px] flex-wrap lg:flex-nowrap gap-y-[10px] w-full lg:w-[calc(100%-450px)] lg:-mt-[30px] backdrop-blur-[15px] pt-[29px] lg:fixed lg: top-[99px] gap-x-[20px] lg:gap-x-[54px] mr-[31px] bg-brand-profile-header border-t-0 border-[3px] border-x-0 lg:border-b-brand-300">
+          {exploreSections?.map((section, index) => (
+            <div
+              key={index}
+              onClick={() => setCurrentSection(index + 1)}
+              className={`border-t-0 border-[3px] border-x-0 z-[22] -mb-[3px] lg:-mb-[3px] ${
+                currentSection === index + 1
+                  ? "border-b-brand-2250"
+                  : "border-b-brand-300"
+              } cursor-pointer`}
+            >
+              <h3
+                className={`${
+                  currentSection === index + 1
+                    ? "text-brand-2250"
+                    : "text-brand-2200"
+                } mb-[11px] text-[11px] lg:text-[14px] font-semibold`}
+              >
+                {section.title}
+              </h3>
+            </div>
+          ))}
+        </div>
+        <div className="w-[100%] px-[31px] mt-[53px] flex flex-wrap gap-[12px] pb-[100px] lg:pb-0">
           {[1, 1, 1, 1, 1]?.map((_, index) => (
             <div key={index} className="relative w-[100%] md:w-[47%] h-[464px]">
               <video
