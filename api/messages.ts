b7aa85096e17b4d3cab323d50bde2206de2b291b
emmydee47 <emmydee47@gmail.com>
2023-10-28 12:47:51 +0100

    added messages endpoint

---
+++ api/messages.ts
@@ -0,0 +1,240 @@
+import { useQuery, useMutation, useInfiniteQuery } from "@tanstack/react-query";
+import { axios } from "@/libs/axios";
+
+// export const useGetNewsfeed = ({ token }: { token: string }) =>
+//   useInfiniteQuery(
+//     ["getNewsfeed", token],
+//     ({ pageParam = 1 }) =>
+//       axios
+//         .get(`/post/newsfeed/personalized?page=${pageParam || 1}`, {
+//           headers: { Authorization: "Bearer " + token },
+//         })
+//         .then((res) => res.data?.results)
+//         .catch((err) => {
+//           throw err.response.data;
+//         }),
+//     {
+//       getNextPageParam: (lastPage, allPages) => {
+//         const nextPage =
+//           lastPage.length === 20 ? allPages.length + 1 : undefined;
+//         return nextPage;
+//       },
+//     }
+//   );
+
+// export const useGetAllPolls = ({ token }: { token: string }) =>
+//   useInfiniteQuery(
+//     ["getPolls", token],
+//     ({ pageParam = 1 }) =>
+//       axios
+//         .get(`/poll?page=${pageParam || 1}`, {
+//           headers: { Authorization: "Bearer " + token },
+//         })
+//         .then((res) => res.data?.results)
+//         .catch((err) => {
+//           throw err.response.data;
+//         }),
+//     {
+//       getNextPageParam: (lastPage, allPages) => {
+//         const nextPage =
+//           lastPage.length === 20 ? allPages.length + 1 : undefined;
+//         return nextPage;
+//       },
+//     }
+//   );
+
+export const getChannel = async (token:string, receiver:string) => {
+  return await axios
+  .get("/auth/users/chat_connect/?receiver="+receiver, {
+    headers: { Authorization: "Bearer " + token },
+  });
+};
+
+
+
+export const useGetChatChannel = ({
+  token,
+  receiver,
+}: {
+  token: string;
+  receiver: string;
+}) =>
+  useQuery(["getChatChannel", token], () =>
+    axios
+      .get("/auth/users/chat_connect/?receiver="+receiver, {
+        headers: { Authorization: "Bearer " + token },
+      })
+      .then((res) => res.data)
+      .catch((err) => {
+        throw err.response.data;
+      })
+  );
+
+export const useGetAllCommentsOnPost = ({
+  token,
+  postId,
+}: {
+  token: string;
+  postId: string;
+}) =>
+  useQuery(
+    ["getAllCommentsOnPost", token, postId],
+    () =>
+      axios
+        .get(`/post/${postId}/comments/`, {
+          headers: { Authorization: "Bearer " + token },
+        })
+        .then((res) => res.data)
+        .catch((err) => {
+          throw err.response.data;
+        }),
+    { refetchOnMount: false, enabled: !!postId }
+  );
+
+interface IPost {
+  postId?: string;
+  token: string;
+  body?: any;
+  additionalHeaders?: any;
+}
+export const useLikeUnlikePost = () =>
+  useMutation(({ postId, token }: IPost) =>
+    axios
+      .post(
+        "/post/like/",
+        { post: postId },
+        { headers: { Authorization: "Bearer " + token } }
+      )
+      .then((res) => res.data)
+      .catch((err) => {
+        throw err.response.data;
+      })
+  );
+
+export const useCommentOnPost = () =>
+  useMutation(({ postId, token, body }: IPost) =>
+    axios
+      .post(
+        "/post/comment/",
+        { post: postId, body: body },
+        { headers: { Authorization: "Bearer " + token } }
+      )
+      .then((res) => res.data)
+      .catch((err) => {
+        throw err.response.data;
+      })
+  );
+
+export const useGetAllHashtags = (token: string) =>
+  useQuery(["getAllHashtags", token], () =>
+    axios
+      .get("/post/hashtag", {
+        headers: { Authorization: "Bearer " + token },
+      })
+      .then((res) => res.data)
+      .catch((err) => {
+        throw err.response.data;
+      })
+  );
+
+export const useCreateHashtag = () =>
+  useMutation(({ token, body }: IPost) =>
+    axios
+      .post("/post/hashtag/", body, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => res.data)
+      .catch((err) => {
+        throw err.response.data;
+      })
+  );
+
+
+  interface IMessage {
+    token: string
+    sender: string;
+    receiver: string;
+    message: string;
+    type: string;
+    channel: string;
+  }
+
+export const useCreateMessage = () =>
+  useMutation(({ token, sender, receiver, message, type, channel }: IMessage) =>
+    axios
+      .post("/send_message/", {
+        sender,
+        receiver,
+        message,
+        type,
+        channel
+      }, {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => res.data)
+      .catch((err) => {
+        throw err.response.data;
+      })
+  );
+
+export const useGetSuggestedFollows = (token: string) =>
+  useQuery(
+    ["getSuggestedFollows", token],
+    () =>
+      axios
+        .get("/auth/users/suggested_follows/", {
+          headers: { Authorization: "Bearer " + token },
+        })
+        .then((res) => res.data)
+        .catch((err) => {
+          throw err.response.data;
+        }),
+    { refetchOnMount: false, retry: 1 }
+  );
+
+export const useGetMyFollowers = ({
+  token,
+  id,
+}: {
+  token: string;
+  id: string;
+}) =>
+  useQuery(
+    ["getMyFollowers", token, id],
+    () =>
+      axios
+        .get(`/auth/users/${id}/followers`, {
+          headers: { Authorization: "Bearer " + token },
+        })
+        .then((res) => res.data)
+        .catch((err) => {
+          throw err.response.data;
+        }),
+    { refetchOnMount: false, retry: 1 }
+  );
+
+export const useGeneralSearch = ({
+  token,
+  search_query,
+}: {
+  token: string;
+  search_query: string;
+}) =>
+  useQuery(
+    ["getSearchValues", token, search_query],
+    () =>
+      axios
+        .get(`/explore?search_key=${search_query}`, {
+          headers: { Authorization: "Bearer " + token },
+        })
+        .then((res) => res.data)
+        .catch((err) => {
+          throw err.response.data;
+        }),
+    { refetchOnMount: false }
+  );
