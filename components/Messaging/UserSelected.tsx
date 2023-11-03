7473f29a3277ef26a6857bc971500489b97d208b
Adejoke <adejokeogunyinka@enyata.com>
2023-11-03 07:41:59 +0100

    fix: fixed message being returned more than once

--- components/Messaging/UserSelected.tsx
+++ components/Messaging/UserSelected.tsx
@@ -6,17 +6,12 @@ import CircleIcon from "@/assets/svgFiles/Circle.svg.next";
 import SendMessageIcon from "@/assets/svgFiles/SendMessage.svg.next";
 import MessageComponent from "./MessageComponent";
 import SearchBar from "../SearchBar";
-import { useEffect, useState } from "react";
-import {getChannel} from "@/api/messages";
-import {
-  setChatChannel
-} from "@/store/slices/messagingSlice";
-
-
-
+import { useEffect, useRef, useState } from "react";
+import { useGetChatChannel } from "@/api/messages";
+import { setChatChannel } from "@/store/slices/messagingSlice";
 
 interface IMessage {
-  id: string
+  id: string;
   message: string;
   img: string;
   time: string;
@@ -24,114 +19,78 @@ interface IMessage {
 
 const UserSelected = () => {
   const { messageUserInfo } = useTypedSelector((state) => state.messaging);
-  const { userInfo } = useTypedSelector((state) => state.profile);
-  const [connected, setConnected] = useState(false);
-  const [websock, setWebSock] = useState<any>()
-  const [messages, setMessages] = useState<IMessage[]>([])
-  const [message, setMessage] = useState<IMessage>()
-  const [messageComponent, setMessageComponent] = useState(<></>)
-  const [msgDisplayed, setMsgDisplayed] = useState(false)
+  const [websock, setWebSock] = useState<any>();
+  const [messages, setMessages] = useState<Record<string, IMessage>[]>([]);
+  const [message, setMessage] = useState<IMessage | string>("");
+  const messagesEndRef = useRef<any>(null);
 
-  let key = 0;
-
-  
   const { data: session } = useSession();
- // const {  data: ChatChannel } = useGetChatChannel(userId);
-  
-  const startChat = async()=>{
-    const token = session?.user?.access
-     const userId = messageUserInfo.id
-     const channel = await getChannel(token || "", userId)
-    //console.log(channel, "Channel Data")
-    setChatChannel(channel)
-    const ws = new WebSocket("ws://143.244.179.156:8000/ws/chat/emmy_lobby/");
-      ws.onopen = (event) => {
-       console.log("Connected!!!")
-       setConnected(true)
-       setWebSock(ws)
-      };
-      ws.onmessage = function (event) {
-       const json = JSON.parse(event.data);
-       const msgs = messages
-       messages.push(event.data)
-      setMessages(messages)
-
-  
-
-// const uniquemsgs = messages.map(item => item.id)
-// .filter((value, index, self) => self.indexOf(value) === index);
-
-// console.log(uniquemsgs, "Unique messages")
-
-
-      // const fmsgs = uniquemsgs.map((newmsg)=>{
-        
-        // if(msgDisplayed===false){
-        //   setMsgDisplayed(true)
-        //   return <MessageComponent
-        //   userImg={`https://res.cloudinary.com/dblqvlycm/image/upload/v1/media/users/user/templates/images/${newmsg?.img}`}
-        //   isReceiver
-        //   time={newmsg.time}
-        //   status="sent"
-        //   key={id}
-        // >
-        //   <Text>{newmsg.message} </Text>
-        // </MessageComponent>
-        // }
-        
-      // })
-      if(messages){
-
-        const result = Object.values(
-          messages.reduce((msg, obj) => ({ ...msg, [obj.id]: obj }), {})
-      );
-    
-        console.log(result, "Unique messages")
-        }
-      
-      console.log(messages, "console data...")
-      
-    //  setMessages(msgs)
-
-    
-
-       
-      };
-
-  
-    
-  }
+  const token = session?.user?.access;
+  const { data: channel } = useGetChatChannel({
+    token: token as string,
+    receiver: messageUserInfo?.id,
+  });
 
   const handleSendMessage = () => {
     let date = new Date();
     let hr = date.getHours();
     let mnt = date.getMinutes();
-    const ampm = hr >= 12 ? 'pm' : 'am';
-    hr = hr > 12 ? hr-12 : hr
-    const hour = hr < 10?`0${hr}`:hr;
-    const minutes = mnt < 10?`0${mnt}`:mnt
+    const ampm = hr >= 12 ? "pm" : "am";
+    hr = hr > 12 ? hr - 12 : hr;
+    const hour = hr < 10 ? `0${hr}` : hr;
+    const minutes = mnt < 10 ? `0${mnt}` : mnt;
 
-    websock.send(JSON.stringify({
-      'message': {
-        'id':Date.now(),
-        'message': message,
-        'img':'user_xtlmxp',
-        'time': `${hour}:${minutes} ${ampm}`
+    websock.send(
+      JSON.stringify({
+        message: {
+          id: Date.now(),
+          message: message,
+          img: session?.user?.image,
+          time: `${hour}:${minutes} ${ampm}`,
+        },
+      })
+    );
 
-      }
-      
-  }));
+    setMessage("");
   };
 
-  const handleTextChange = (e:any)=>{
-    setMessage(e.target.value)
-  }
+  const handleTextChange = (e: any) => {
+    setMessage(e.target.value);
+  };
 
-  useEffect(()=>{
-    if(connected===false)
-    startChat(); 
-    
-  })
+  const url = "ws://143.244.179.156:8000/ws/chat/emmy_lobby/";
+
+  useEffect(() => {
+    const ws = new WebSocket(url);
+
+    ws.onopen = (event) => {
+      setChatChannel(channel);
+      setWebSock(ws);
+    };
+
+    ws.onmessage = function (event) {
+      const json = JSON.parse(event.data);
+      setMessages((prev) => [...prev, json]);
+    };
+
+    return () => {
+      if (ws.readyState === 1) {
+        ws.close();
+      } else {
+        ws.addEventListener("open", () => {
+          ws.close();
+        });
+      }
+    };
+  }, []);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   return (
     <Box w="full" h="full" pos="relative">
@@ -203,21 +162,18 @@ const UserSelected = () => {
         overflowY="scroll"
         padding={{ base: "18px 12px 120px 12px", md: "28px 18px 50px 18px" }}
       >
-        {/* {
-          messages.map((msg)=>{
-             ++key
-             const newmsg = JSON.parse(msg)
-            return <MessageComponent
-            userImg={`https://res.cloudinary.com/dblqvlycm/image/upload/v1/media/users/user/templates/images/${newmsg?.img}`}
+        {messages.map((msg, index) => (
+          <MessageComponent
+            key={index}
             isReceiver
-            time={newmsg.time}
+            userImg={msg?.message?.img}
+            time={msg?.message?.time}
             status="sent"
-            key={key}
           >
-            <Text>{newmsg.message} </Text>
+            <Text fontSize="16px">{msg?.message?.message}</Text>
           </MessageComponent>
-          })
-        } */}
+        ))}
+        <Box ref={messagesEndRef}></Box>
       </Flex>
 
       <Flex
@@ -252,6 +208,7 @@ const UserSelected = () => {
             _focusVisible={{ borderColor: "transparent" }}
             borderRadius="24px"
             onChange={handleTextChange}
+            value={typeof message === "string" ? message : message?.message}
           />
           <Flex
             h="full"
